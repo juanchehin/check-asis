01365d0d14525856170f6d915db8735165218545
chehin <chehinjuan@gmail.com>
2021-12-05 10:40:28 -0300

    Diseño basico nuevo personal

--- check-asis/Presentacion/MenuPrincipal.Designer.cs
+++ check-asis/Presentacion/MenuPrincipal.Designer.cs
@@ -31,22 +31,22 @@ namespace check_asis.Presentacion
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnPlanilla = new System.Windows.Forms.Button();
-            this.btnPersonal = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.btnRegistro = new System.Windows.Forms.Button();
-            this.btnUsuarios = new System.Windows.Forms.Button();
-            this.btnRestaurar = new System.Windows.Forms.Button();
-            this.btnRespaldos = new System.Windows.Forms.Button();
-            this.btnEstaciones = new System.Windows.Forms.Button();
-            this.panel5 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnEstaciones = new System.Windows.Forms.Button();
+            this.btnRespaldos = new System.Windows.Forms.Button();
+            this.btnRestaurar = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.btnPersonal = new System.Windows.Forms.Button();
+            this.btnPlanilla = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -58,44 +58,6 @@ namespace check_asis.Presentacion
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(220, 749);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(438, 71);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(196, 283);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.panel6);
-            this.panel3.Controls.Add(this.btnPersonal);
-            this.panel3.Controls.Add(this.btnPlanilla);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(220, 202);
-            this.panel3.TabIndex = 0;
-            // 
-            // btnPlanilla
-            // 
-            this.btnPlanilla.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPlanilla.Location = new System.Drawing.Point(4, 47);
-            this.btnPlanilla.Name = "btnPlanilla";
-            this.btnPlanilla.Size = new System.Drawing.Size(213, 66);
-            this.btnPlanilla.TabIndex = 0;
-            this.btnPlanilla.Text = "Pre planilla";
-            this.btnPlanilla.UseVisualStyleBackColor = true;
-            // 
-            // btnPersonal
-            // 
-            this.btnPersonal.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPersonal.Location = new System.Drawing.Point(3, 119);
-            this.btnPersonal.Name = "btnPersonal";
-            this.btnPersonal.Size = new System.Drawing.Size(213, 66);
-            this.btnPersonal.TabIndex = 1;
-            this.btnPersonal.Text = "Personal";
-            this.btnPersonal.UseVisualStyleBackColor = true;
             // 
             // panel4
             // 
@@ -111,53 +73,24 @@ namespace check_asis.Presentacion
             this.panel4.Size = new System.Drawing.Size(213, 387);
             this.panel4.TabIndex = 1;
             // 
-            // btnRegistro
+            // label1
             // 
-            this.btnRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistro.Image = ((System.Drawing.Image)(resources.GetObject("btnRegistro.Image")));
-            this.btnRegistro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRegistro.Location = new System.Drawing.Point(-3, 3);
-            this.btnRegistro.Name = "btnRegistro";
-            this.btnRegistro.Size = new System.Drawing.Size(213, 48);
-            this.btnRegistro.TabIndex = 1;
-            this.btnRegistro.Text = "Registro";
-            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Check-asis";
             // 
-            // btnUsuarios
+            // panel5
             // 
-            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuarios.Image")));
-            this.btnUsuarios.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnUsuarios.Location = new System.Drawing.Point(-3, 57);
-            this.btnUsuarios.Name = "btnUsuarios";
-            this.btnUsuarios.Size = new System.Drawing.Size(213, 48);
-            this.btnUsuarios.TabIndex = 2;
-            this.btnUsuarios.Text = "Usuarios";
-            this.btnUsuarios.UseVisualStyleBackColor = true;
-            // 
-            // btnRestaurar
-            // 
-            this.btnRestaurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaurar.Image")));
-            this.btnRestaurar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRestaurar.Location = new System.Drawing.Point(0, 111);
-            this.btnRestaurar.Name = "btnRestaurar";
-            this.btnRestaurar.Size = new System.Drawing.Size(213, 48);
-            this.btnRestaurar.TabIndex = 3;
-            this.btnRestaurar.Text = "Restaurar BD";
-            this.btnRestaurar.UseVisualStyleBackColor = true;
-            // 
-            // btnRespaldos
-            // 
-            this.btnRespaldos.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRespaldos.Image = ((System.Drawing.Image)(resources.GetObject("btnRespaldos.Image")));
-            this.btnRespaldos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRespaldos.Location = new System.Drawing.Point(0, 169);
-            this.btnRespaldos.Name = "btnRespaldos";
-            this.btnRespaldos.Size = new System.Drawing.Size(213, 48);
-            this.btnRespaldos.TabIndex = 4;
-            this.btnRespaldos.Text = "Respaldar BD";
-            this.btnRespaldos.UseVisualStyleBackColor = true;
+            this.panel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel5.BackgroundImage")));
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel5.Location = new System.Drawing.Point(3, 278);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(207, 59);
+            this.panel5.TabIndex = 6;
             // 
             // btnEstaciones
             // 
@@ -171,24 +104,64 @@ namespace check_asis.Presentacion
             this.btnEstaciones.Text = "Estaciones";
             this.btnEstaciones.UseVisualStyleBackColor = true;
             // 
-            // panel5
+            // btnRespaldos
             // 
-            this.panel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel5.BackgroundImage")));
-            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel5.Location = new System.Drawing.Point(3, 278);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(207, 59);
-            this.panel5.TabIndex = 6;
+            this.btnRespaldos.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRespaldos.Image = ((System.Drawing.Image)(resources.GetObject("btnRespaldos.Image")));
+            this.btnRespaldos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRespaldos.Location = new System.Drawing.Point(0, 169);
+            this.btnRespaldos.Name = "btnRespaldos";
+            this.btnRespaldos.Size = new System.Drawing.Size(213, 48);
+            this.btnRespaldos.TabIndex = 4;
+            this.btnRespaldos.Text = "Respaldar BD";
+            this.btnRespaldos.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // btnRestaurar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS Gothic", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 350);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(174, 31);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Check-asis";
+            this.btnRestaurar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("btnRestaurar.Image")));
+            this.btnRestaurar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRestaurar.Location = new System.Drawing.Point(0, 111);
+            this.btnRestaurar.Name = "btnRestaurar";
+            this.btnRestaurar.Size = new System.Drawing.Size(213, 48);
+            this.btnRestaurar.TabIndex = 3;
+            this.btnRestaurar.Text = "Restaurar BD";
+            this.btnRestaurar.UseVisualStyleBackColor = true;
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("btnUsuarios.Image")));
+            this.btnUsuarios.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUsuarios.Location = new System.Drawing.Point(-3, 57);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(213, 48);
+            this.btnUsuarios.TabIndex = 2;
+            this.btnUsuarios.Text = "Usuarios";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.Image = ((System.Drawing.Image)(resources.GetObject("btnRegistro.Image")));
+            this.btnRegistro.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegistro.Location = new System.Drawing.Point(-3, 3);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(213, 48);
+            this.btnRegistro.TabIndex = 1;
+            this.btnRegistro.Text = "Registro";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.btnPersonal);
+            this.panel3.Controls.Add(this.btnPlanilla);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(220, 202);
+            this.panel3.TabIndex = 0;
             // 
             // panel6
             // 
@@ -197,28 +170,61 @@ namespace check_asis.Presentacion
             this.panel6.Size = new System.Drawing.Size(57, 38);
             this.panel6.TabIndex = 2;
             // 
+            // btnPersonal
+            // 
+            this.btnPersonal.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPersonal.Location = new System.Drawing.Point(3, 119);
+            this.btnPersonal.Name = "btnPersonal";
+            this.btnPersonal.Size = new System.Drawing.Size(213, 66);
+            this.btnPersonal.TabIndex = 1;
+            this.btnPersonal.Text = "Personal";
+            this.btnPersonal.UseVisualStyleBackColor = true;
+            // 
+            // btnPlanilla
+            // 
+            this.btnPlanilla.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlanilla.Location = new System.Drawing.Point(4, 47);
+            this.btnPlanilla.Name = "btnPlanilla";
+            this.btnPlanilla.Size = new System.Drawing.Size(213, 66);
+            this.btnPlanilla.TabIndex = 0;
+            this.btnPlanilla.Text = "Pre planilla";
+            this.btnPlanilla.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(584, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 63);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bienvenidos";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(907, 749);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Check-Asis";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Button btnEstaciones;
         private System.Windows.Forms.Button btnRespaldos;
@@ -231,5 +237,6 @@ namespace check_asis.Presentacion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label2;
     }
 }
