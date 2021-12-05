00f08cfb1a8025ec82350958ee58df4839546fa1
chehin <chehinjuan@gmail.com>
2021-12-05 11:46:15 -0300

    Conexion maestra

---
+++ check-asis/Datos/ConexionMaestra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_asis.Datos
+{
+    public class ConexionMaestra
+    {
+        public static string conexion = @"Data source=DESKTOP-9BKC47G\SQLEXPRESS;Initial Catalog=check;Integrated Security=true";
+        public static SqlConnection conectar = new SqlConnection(conexion);
+        public static void abrir()
+        {
+            if (conectar.State == ConnectionState.Closed)
+            {
+                conectar.Open();
+            }
+        }
+        public static void cerrar()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
---
+++ check-asis/Datos/Dpersonal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using check_asis.Logica;
+using System.Windows.Forms;
+
+namespace check_asis.Datos
+{
+    public class Dpersonal
+    {
+		public bool InsertarPersonal(Lpersonal parametros)
+		{
+			try
+			{
+				CONEXIONMAESTRA.abrir();
+				SqlCommand cmd = new SqlCommand("InsertarPersonal", CONEXIONMAESTRA.conectar);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@Nombres", parametros.Nombres);
+				cmd.Parameters.AddWithValue("@Identificacion", parametros.Identificacion);
+				cmd.Parameters.AddWithValue("@Pais", parametros.Pais);
+				cmd.Parameters.AddWithValue("@Id_cargo", parametros.Id_cargo);
+				cmd.Parameters.AddWithValue("@SueldoPorHora", parametros.SueldoPorHora);
+				cmd.ExecuteNonQuery();
+				return true;
+			}
+			catch (Exception ex)
+			{
+
+				MessageBox.Show(ex.Message);
+				return false;
+			}
+			finally
+			{
+				CONEXIONMAESTRA.cerrar();
+			}
+		}
+	}
+}
