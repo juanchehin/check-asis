33b1e9e5c368cc09c904eedeb73701d23f6664a6
chehin <chehinjuan@gmail.com>
2021-12-31 14:12:18 -0300

    diseño panel} de instalacion de BD

--- check-asis/Presentacion/asistente-instalacion/InstalacionBD.Designer.cs
+++ check-asis/Presentacion/asistente-instalacion/InstalacionBD.Designer.cs
@@ -30,32 +30,274 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstalacionBD));
             this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtCrear_procedimientos = new System.Windows.Forms.RichTextBox();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtEliminarBase = new System.Windows.Forms.RichTextBox();
+            this.txtArgumentosini = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.GroupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel5
             // 
             this.panel5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel5.BackgroundImage")));
             this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel5.Location = new System.Drawing.Point(12, 12);
+            this.panel5.Location = new System.Drawing.Point(288, 109);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(88, 69);
+            this.panel5.Size = new System.Drawing.Size(218, 177);
             this.panel5.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Instalacion del servidor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(208, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "No cierre esta ventana, se cerrara automaticamente";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 323);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.txtArgumentosini);
+            this.panel1.Controls.Add(this.GroupBox1);
+            this.panel1.Controls.Add(this.GroupBox2);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(39, 362);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(936, 732);
+            this.panel1.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre de la instancia : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Usuario :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Contraseña :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Base de datos : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Nombre del script : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(134, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(134, 103);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(134, 132);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.txtCrear_procedimientos);
+            this.GroupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox2.ForeColor = System.Drawing.Color.Black;
+            this.GroupBox2.Location = new System.Drawing.Point(6, 180);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(446, 183);
+            this.GroupBox2.TabIndex = 623;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Pega tu Script para Crear las Tablas y Procedimientos";
+            // 
+            // txtCrear_procedimientos
+            // 
+            this.txtCrear_procedimientos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCrear_procedimientos.Location = new System.Drawing.Point(6, 32);
+            this.txtCrear_procedimientos.Name = "txtCrear_procedimientos";
+            this.txtCrear_procedimientos.Size = new System.Drawing.Size(434, 145);
+            this.txtCrear_procedimientos.TabIndex = 589;
+            this.txtCrear_procedimientos.Text = resources.GetString("txtCrear_procedimientos.Text");
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.txtEliminarBase);
+            this.GroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GroupBox1.ForeColor = System.Drawing.Color.Black;
+            this.GroupBox1.Location = new System.Drawing.Point(464, 180);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(316, 154);
+            this.GroupBox1.TabIndex = 624;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Script para Eliminar la base de datos";
+            // 
+            // txtEliminarBase
+            // 
+            this.txtEliminarBase.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEliminarBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEliminarBase.Location = new System.Drawing.Point(3, 16);
+            this.txtEliminarBase.Name = "txtEliminarBase";
+            this.txtEliminarBase.Size = new System.Drawing.Size(310, 135);
+            this.txtEliminarBase.TabIndex = 589;
+            this.txtEliminarBase.Text = "alter database BASEADACURSO set single_user with rollback immediate \nDROP DATABAS" +
+    "E BASEADACURSO";
+            // 
+            // txtArgumentosini
+            // 
+            this.txtArgumentosini.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArgumentosini.Location = new System.Drawing.Point(12, 369);
+            this.txtArgumentosini.Name = "txtArgumentosini";
+            this.txtArgumentosini.Size = new System.Drawing.Size(446, 216);
+            this.txtArgumentosini.TabIndex = 590;
+            this.txtArgumentosini.Text = resources.GetString("txtArgumentosini.Text");
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(464, 369);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(446, 216);
+            this.richTextBox1.TabIndex = 625;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
             // InstalacionBD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(823, 389);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "InstalacionBD";
-            this.Text = "InstalacionBD";
+            this.Text = "Instalacion de la base de datos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.RichTextBox richTextBox1;
+        internal System.Windows.Forms.RichTextBox txtArgumentosini;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.RichTextBox txtEliminarBase;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.RichTextBox txtCrear_procedimientos;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
