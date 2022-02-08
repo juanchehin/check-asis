905d0f851c8bcee1b155cd3b02decb2c16245d71
chehin <chehinjuan@gmail.com>
2022-02-08 11:06:17 -0300

    encriptacion conexion

---
+++ check-asis/Logica/Desencriptar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace check_asis.Logica
+{
+    public class Desencryptacion
+    {
+        static private AES aes = new AES();
+        static public string CnString;
+        static string dbcnString;
+        static public string appPwdUnique = "check-asis.slider";
+        public static object checkServer()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("ConnectionString.xml");
+            XmlElement root = doc.DocumentElement;
+            dbcnString = root.Attributes[0].Value;
+            CnString = (aes.Decrypt(dbcnString, appPwdUnique, int.Parse("256")));
+            return CnString;
+        }
+
+
+    }
+}
